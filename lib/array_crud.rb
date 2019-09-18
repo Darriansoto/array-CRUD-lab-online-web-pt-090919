c1c88b7c083c8a9137c0148a301de887a1d05434
Darrian Soto <ddog350@gmail.com>
2019-09-18 04:51:59 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -19,8 +19,9 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  array = [1,2,3,4,"arrays"]
-  arrays = array.pop
+  x = "arrays!"
+  array = [1,2,3,4,x]
+  x = array.pop
 end
 
 def remove_element_from_start_of_array(array)
